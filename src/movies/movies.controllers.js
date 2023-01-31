d33b042bc8f21500e0a826e5c7c23db876f1bf6a
Sh3yk0  (Sahid Kick) <sheyko.wk@gmail.com>
2023-01-30 21:06:05 -0600

    add: movie services and controllers

--- src/movies/movies.controllers.js
+++ src/movies/movies.controllers.js
@@ -7,10 +7,16 @@ const findAllMovies = async () => {
     return await moviesDB
 }
 
+//* findAllMovies()
+
+
 const findMovieById = async (id) => {
     const filteredMovie = await moviesDB.find(movie => movie.id === id) 
     return filteredMovie
 }
+
+//* findMovieById(3)
+
 
 const createNewMovie = async (movieObj) => {
     const newMovie = {
@@ -26,8 +32,15 @@ const createNewMovie = async (movieObj) => {
     return newMovie
 }
 
+//* createNewMovie({
+//*     title: 'Harry Potter',
+//*     synopsis: 'Un maguito guay',
+//*     director: 'JK',
+//*     year: 2007
+//* })
+
 module.exports = {
     findAllMovies,
     findMovieById,
-    createNewMovie
+    createNewMovie,
 }
